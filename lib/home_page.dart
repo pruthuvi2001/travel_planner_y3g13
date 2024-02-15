b1fdd3f9e06fd7e21af327613dc04dbbea27de8e
Muzni-Munas <munas.20212065@iit.ac.lk>
2024-02-15 14:39:09 +0530

    updated widget designs

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
               text: 'Travel Planner',
               style: TextStyle(
                   color: Colors.black,
-                  fontSize: 35,
+                  fontSize: 33,
                   fontWeight: FontWeight.bold),
               children: [
                 TextSpan(
@@ -54,9 +54,10 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Container(
+        padding:EdgeInsets.only(left: 20.0, right: 20.0),
         child: ListView(
           padding: EdgeInsets.only(
-            top: 20.0,
+            top: 25.0,
             bottom: 20.0,
           ),
           children: [
@@ -72,120 +73,131 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            SizedBox(height: 30),
+            SizedBox(height: 25),
 // Search for Places text
-            Center(
-              child: Text(
-                'Search for Places',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
+            Container(
+              height: 350.0,
+              decoration: BoxDecoration(
+                color: Colors.grey[200]?.withOpacity(0.8),
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(15),
               ),
-            ),
-            SizedBox(height: 25),
-// Starting point
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: Text(
-                'Starting Point',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.0,
-                ),
-              ),
-            ),
-            SizedBox(height: 7),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                height: 40,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200]?.withOpacity(0.8),
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 15.0, top: 15.0),
-                  child: TextField(
-                    controller: startingPointController,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18.0,
-                    ),
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Starting Point',
-                      hintStyle: TextStyle(color: Colors.grey),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-// Ending point
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: Text(
-                'Destination',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18.0,
-                ),
-              ),
-            ),
-            SizedBox(height: 7),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                height: 40,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200]?.withOpacity(0.8),
-                  border: Border.all(color: Colors.white),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 15.0, top: 15.0),
-                  child: TextField(
-                    controller: endingPointController,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18.0,
-                    ),
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Ending Point',
-                      hintStyle: TextStyle(color: Colors.grey),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 25),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40.0),
-              child: ElevatedButton(
-                onPressed: _searchPlace,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-                child: Container(
-                  height: 45,
-                  child: Center(
+              child: ListView(
+                padding: EdgeInsets.only(top:15.0),
+                children: [
+                  Center(
                     child: Text(
-                      'Search Places',
+                      'Search for Places',
                       style: TextStyle(
                         color: Colors.black,
-                        fontSize: 18,
+                        fontSize: 30.0,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ),
+                  SizedBox(height: 25),
+// Starting point
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Text(
+                      'Starting Point',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 7),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200]?.withOpacity(0.8),
+                        border: Border(bottom: BorderSide(color: Colors.grey)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 15.0, top: 15.0),
+                        child: TextField(
+                          controller: startingPointController,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                          ),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Starting Point',
+                            hintStyle: TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+// Ending point
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Text(
+                      'Destination',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 7),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200]?.withOpacity(0.8),
+                        border: Border(bottom: BorderSide(color: Colors.grey)),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 15.0, top: 15.0),
+                        child: TextField(
+                          controller: endingPointController,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                          ),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Ending Point',
+                            hintStyle: TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: ElevatedButton(
+                      onPressed: _searchPlace,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.amber,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                      child: Container(
+                        height: 45,
+                        child: Center(
+                          child: Text(
+                            'Search Places',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
@@ -196,8 +208,8 @@ class _HomePageState extends State<HomePage> {
         iconSize: 20.0,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.black,
-        selectedLabelStyle: TextStyle(color: Colors.black),
-        unselectedLabelStyle: TextStyle(color: Colors.black),
+        selectedLabelStyle: TextStyle(color: Colors.black, fontSize: 13.0),
+        unselectedLabelStyle: TextStyle(color: Colors.black, fontSize: 13.0),
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
