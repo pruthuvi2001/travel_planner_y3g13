ace414dd352e7a9574deacceb180699d269c9c7d
ahiyarmohamed215 <95045272+ahiyarmohamed215@users.noreply.github.com>
2024-02-28 12:01:27 +0530

    change password page created

---
+++ lib/pages/Profile_Page/change_password.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class ChangePasswordPage extends StatelessWidget {
+  const ChangePasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+ }
