ed47dac3f3c8d85519fc2b524481d905e2ab5627
ahiyarmohamed215 <95045272+ahiyarmohamed215@users.noreply.github.com>
2024-03-23 14:52:04 +0530

    Integrated with firebase

--- lib/pages/Profile_Page/change_password.dart
+++ lib/pages/Profile_Page/change_password.dart
@@ -1,76 +1,133 @@
-
 import 'package:flutter/material.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
-import 'package:travel_planner_y3g13/pages/Profile_Page/profile_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-import 'edit_profile.dart';
-
-class ChangePasswordPage extends StatelessWidget {
+class ChangePasswordPage extends StatefulWidget {
   const ChangePasswordPage({super.key});
+
+  @override
+  _ChangePasswordPageState createState() => _ChangePasswordPageState();
+}
+
+class _ChangePasswordPageState extends State<ChangePasswordPage> {
+  final TextEditingController _oldPasswordController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         backgroundColor: Colors.amber,
         leading: IconButton(
-            onPressed: () => {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ProfilePage()))
-                },
-            icon: const Icon(LineAwesomeIcons.angle_left)),
-        title: Text("Change Password",
-            style: Theme.of(context).textTheme.headlineMedium),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(LineAwesomeIcons.angle_left),
+        ),
+        title: Text(
+          "Change Password",
+          style: Theme.of(context).textTheme.headline6,
+        ),
       ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(children: [
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
             const SizedBox(height: 50),
             Form(
-                child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                      label: Text("Old Password"),
-                      prefixIcon: Icon(LineAwesomeIcons.eye)),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      label: Text("New Password"),
-                      prefixIcon: Icon(LineAwesomeIcons.eye_slash)),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      label: Text("Confirm New Password"),
-                      prefixIcon: Icon(LineAwesomeIcons.eye_slash)),
-                ),
-                const SizedBox(height: 20),
-              ],
-            )),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                onPressed: () => {
-                  Navigator.push(context,MaterialPageRoute(builder:(context) => const EditProfilePage()))
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amber,
-                    side: BorderSide.none,
-                    shape: const StadiumBorder()),
-                child: Text("Save New Password",
-                    style: Theme.of(context).textTheme.bodyMedium),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _oldPasswordController,
+                    decoration: const InputDecoration(
+                      labelText: "Old Password",
+                      prefixIcon: Icon(LineAwesomeIcons.eye),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _newPasswordController,
+                    decoration: const InputDecoration(
+                      labelText: "New Password",
+                      prefixIcon: Icon(LineAwesomeIcons.eye_slash),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _confirmPasswordController,
+                    decoration: const InputDecoration(
+                      labelText: "Confirm New Password",
+                      prefixIcon: Icon(LineAwesomeIcons.eye_slash),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20),
+                ],
               ),
             ),
-          ]
-          ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  changePassword();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  "Save New Password",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  Future<void> changePassword() async {
+    // Get the current user
+    User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      // Verify old password
+      try {
+        // Reauthenticate user before changing password
+        AuthCredential credential = EmailAuthProvider.credential(
+          email: user.email!,
+          password: _oldPasswordController.text,
+        );
+        await user.reauthenticateWithCredential(credential);
+
+        // Update password
+        await user.updatePassword(_newPasswordController.text);
+
+        // Show a snackbar to indicate success
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Password changed successfully'),
+          ),
+        );
+      } catch (e) {
+        // Handle authentication or password update failure
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Failed to change password. Please try again.'),
+          ),
+        );
+      }
+    }
   }
 }
